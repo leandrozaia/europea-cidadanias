bfd1285a489051d34338d6e4f0d863fb1d08c69c
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-10-27 18:48:14 -0300

    feat: test 1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="pt-BR">
+    <Html>
       <Head>
         <link rel="icon" href="#" />
         <meta name="description" content="Europea Cidadanias" />
