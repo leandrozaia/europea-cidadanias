942663539a3b161e95dbdbb3b249d6dfd40118b0
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-11-08 18:05:27 -0300

    feat: add improvements to faq component

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -48,36 +48,19 @@ export function FAQ({ faqs }: Props) {
           <div className="absolute top-0 w-full h-full bg-main opacity-90 rounded-[32px]" />
 
           {/* Content */}
-          <div className="absolute top-0 w-full h-full p-8 z-10">
-            <h3 className="text-white font-bold text-xl sm:text-2xl mb-1 sm:mb-6">Tudo o que você precisa saber sobre a Europea cidadanias</h3>
-            <p className="text-base text-white mb-1">Ainda tem alguma dúvida?</p>
-            <p className="text-base text-white">Fale conosco pelo
-              <a
-                href={`https://api.whatsapp.com/send?phone=5511970746016`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="ml-1 underline underline-offset-4"
-              >
-                WhatsApp
-              </a>
-            </p>
+          <div className="absolute top-0 w-full h-full z-10 flex flex-col items-end justify-between">
+            <h3 className="text-white font-bold text-xl sm:text-2xl mx-8 mt-8">
+              {faq.title}
+            </h3>
             <Image
               src="/leandro.png"
               alt="Imagem do coliseu"
               width={0}
               height={0}
               sizes="100vw"
-              className="block lg:hidden w-auto h-[388px] sm:h-[412px] absolute -bottom-12 right-0"
+              className="w-auto h-[380px] sm:h-[400px] lg:h-[450px] mr-5"
             />
           </div>
-          <Image
-            src="/leandro.png"
-            alt="Imagem do coliseu"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className="hidden lg:block absolute -bottom-24 -right-12 w-auto h-[512px]"
-          />
         </div>
 
         <div className="w-full flex flex-col gap-6">
