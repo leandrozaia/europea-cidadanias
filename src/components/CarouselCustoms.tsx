49f4ddf8e8009f46d86f58554d2594ab74ef62b0
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-12-17 18:53:59 -0300

    feat: add arrow to mobile services

--- src/components/CarouselCustoms.tsx
+++ src/components/CarouselCustoms.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md'
 
 export const CustomPrevArrow = ({ onClick, custom }: any) => (
-  <div className={`text-white opacity-50 text-4xl md:text-6xl absolute top-[50%] translate-y-[-50%] cursor-pointer z-10 left-0 md:-left-8 lg:-left-12 ${custom}`} onClick={onClick}>
+  <div className={`text-main md:text-white opacity-50 text-4xl md:text-6xl absolute top-[50%] translate-y-[-50%] cursor-pointer z-10 left-1 md:-left-8 lg:-left-12 ${custom}`} onClick={onClick}>
     <MdKeyboardArrowLeft />
   </div>
 )
 
 export const CustomNextArrow = ({ onClick, custom }: any) => (
-  <div className={`text-white opacity-50 text-4xl md:text-6xl absolute top-[50%] translate-y-[-50%] cursor-pointer z-10 right-0 md:-right-8 lg:-right-12 ${custom}`} onClick={onClick}>
+  <div className={`text-main md:text-white opacity-50 text-4xl md:text-6xl absolute top-[50%] translate-y-[-50%] cursor-pointer z-10 right-1 md:-right-8 lg:-right-12 ${custom}`} onClick={onClick}>
     <MdKeyboardArrowRight />
   </div>
 )
