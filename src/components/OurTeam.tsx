604f3945f3d2aa6d33c5a9c9b80eb3e4ebf731f1
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2024-09-27 11:52:03 -0300

    feat: add certifieds section

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -1,6 +1,7 @@
 import { Member } from "@/components/Member"
 import { useRouter } from "next/router"
 import { translations } from "@/utils/translations"
+import Image from "next/image"
 
 interface MemberProps {
   profilePicture: {
@@ -28,6 +29,12 @@ interface Props {
 export function OurTeam({ members }: Props) {
   const { locale } = useRouter()
   const { ourTeam } = translations[locale as string]
+  const certifieds = [
+    '/certified_01.jpeg',
+    '/certified_02.jpeg',
+    '/certified_03.jpeg',
+    '/certified_04.jpeg'
+  ]
 
   return (
     <section className="pb-12 px-4 xl:px-0">
@@ -36,10 +43,31 @@ export function OurTeam({ members }: Props) {
           {ourTeam.title}
         </h2>
 
-        <div className="max-w-4xl mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="max-w-4xl mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-6 mb-12">
           {members.map((member: MemberProps) => (
             <Member key={member.name} member={member} />
           ))}
+        </div>
+
+        {/* Certifieds */}
+        <div className="max-w-3xl mx-auto">
+          <h3 className="text-main text-3xl font-bold uppercase text-center mb-2">
+            Nossas Certificações
+          </h3>
+          <p className="text-main text-center mb-12">
+            De: Leandro Zaia Filho
+          </p>
+          
+          <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {certifieds.map((certified) => <Image
+              src={certified}
+              alt="Certificado"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-40 h-40"
+            />)}
+          </div>
         </div>
       </div>
     </section>
