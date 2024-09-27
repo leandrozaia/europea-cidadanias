5cefa32e2af70096e9d88ca9d10554ccab2de1d1
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2024-09-27 11:55:11 -0300

    fix: add key prop to image element

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -60,6 +60,7 @@ export function OurTeam({ members }: Props) {
           
           <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
             {certifieds.map((certified) => <Image
+              key={certified}
               src={certified}
               alt="Certificado"
               width={0}
